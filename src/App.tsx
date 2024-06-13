6ffd4e355d79260bc31ada2eb9f783314698a187
zhuddan <yobaci@163.com>
2024-06-13 15:02:33 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <button type="button" className="bg-red-200 font-bold">
+        132
+      </button>
+    </>
+  )
+}
+
+export default App
