7523aa80ccf9f31b86167101bfd0b24902d50b88
zhuddan <yobaci@163.com>
2024-06-21 18:02:21 +0800

    fix

--- src/components/buttons/player-playlist-button.tsx
+++ src/components/buttons/player-playlist-button.tsx
@@ -1,11 +1,22 @@
+import { useToggle } from 'react-use'
 import Button from '../button'
+import { Modal } from '../modal'
 import MdiPlaylistMusicOutline from '~icons/mdi/playlist-music-outline'
 
 export function PlayerPlaylistButton() {
+  const [show, toggle] = useToggle(false)
+
   return (
-    <Button
-      icon={<MdiPlaylistMusicOutline />}
-    >
-    </Button>
+    <>
+      <Button
+        icon={<MdiPlaylistMusicOutline />}
+        onClick={toggle}
+      >
+      </Button>
+      <Modal show={show} onClose={() => toggle(false)}>
+        <img src="/public/cover/The Clouds in Camarillo.jpg" alt="" />
+      </Modal>
+    </>
+
   )
 }
---
+++ src/components/modal.tsx
@@ -0,0 +1,80 @@
+import { useEffect, useRef, useState } from 'react'
+import { useClickAway } from 'react-use'
+
+type ModelType = 'top' | 'bottom' | 'center'
+export function Modal({
+  show,
+  children,
+  onClose,
+  type = 'center',
+}: React.PropsWithChildren<{
+  show?: boolean
+  onClose?: () => void
+  type?: ModelType
+}>) {
+  const ref = useRef<HTMLDivElement>(null)
+  const [className, setClassName] = useState('')
+  const [maskClassName, setMaskClassName] = useState('')
+  const [maskStyle, setMaskStyle] = useState<React.CSSProperties>({
+    transitionProperty: 'opacity,transform',
+  })
+  const duration = 0
+  useEffect(() => {
+    if (show) {
+      setClassName(type === 'center' ? 'scale-100' : 'translate-y-0')
+      console.log(type === 'center' ? 'scale-100' : 'translate-y-0')
+      setMaskClassName('opacity-100 translate-y-0')
+      setMaskStyle(style => ({
+        ...style,
+        transitionDuration: `${duration}ms, 0s`,
+        transitionDelay: '0s, 0s',
+      }))
+    }
+    else {
+      setClassName('')
+      setMaskClassName('')
+      setMaskStyle(style => ({
+        ...style,
+        transitionDuration: `${duration}ms, 0s`,
+        transitionDelay: `0s, ${duration}ms`,
+      }))
+    }
+  }, [show, type])
+  useClickAway(ref, () => {
+    onClose?.()
+  })
+  let baseClass = ''
+  if (type === 'bottom') {
+    baseClass = 'bottom-0 translate-y-full'
+  }
+  else if (type === 'top') {
+    baseClass = 'top-0  -translate-y-full'
+  }
+  else if (type === 'center') {
+    baseClass = 'top-0 bottom-0 scale-0 my-auto'
+  }
+  return (
+    <>
+      <div
+        className={`
+          mask fixed z-10 inset-0
+          bg-black bg-opacity-30
+          opacity-0 translate-y-full 
+          ${maskClassName}
+        `}
+        style={maskStyle}
+      >
+      </div>
+      <div
+        className={`
+          modal fixed z-20 ${baseClass}
+          transition-transform duration-${duration}
+          ${className}
+        `}
+        ref={ref}
+      >
+        { children}
+      </div>
+    </>
+  )
+}
